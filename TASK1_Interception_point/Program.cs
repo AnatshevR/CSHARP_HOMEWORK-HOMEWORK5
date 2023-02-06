6802e6d89b1286a7fa07dd37f240ba3c5335e6c9
Roman_Anatshev <romananatshev@gmail.com>
2023-02-06 13:29:43 +0300

    Найти точку пересечения двух прямых заданных уравнением

---
+++ TASK1_Interception_point/Program.cs
@@ -0,0 +1,11 @@
+﻿//Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+Console.WriteLine("enter 4 digits ");
+int k1 = int.Parse(Console.ReadLine()!);
+int b1 = int.Parse(Console.ReadLine()!);
+int k2 = int.Parse(Console.ReadLine()!);
+int b2 = int.Parse(Console.ReadLine()!);
+
+int x = (b1-b2)/(k1-k2);
+int y = k1*x +b1;
+
+Console.WriteLine("the point of interception has this coordinates: " + "x= " + x + "; y= " + y);
